80b192116b1355bd7d74a958a5698abaa0e60778
ShougM77 <mdge@hotmail.fr>
2024-08-14 18:56:05 +0200

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Card from '../components/Card';
 import data from '../data/logements.json';
+import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Layout/Footer";
+import Header from "../../components/Layout/Header";
 
 function Home() {
   return (
+    <>
+    <Header />
     <main>
+    <Banner imageSrc={bannerImg} text="Chez vous, partout et ailleurs" />
       <section>
         {data.map((property) => (
           <Card key={property.id} property={property} />
         ))}
       </section>
     </main>
+    <Footer />
+    </>
   );
 }
 
